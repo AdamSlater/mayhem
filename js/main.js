2ad7cf523fd9506ad161346d94943fa65230cc14
Adam Slater <adam.slater.1010@gmail.com>
2020-10-01 13:35:14 -0700

    added temporary img assets

--- js/main.js
+++ js/main.js
@@ -13,6 +13,14 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
+  this.load.image('sky', 'assets/img/sky.png');
+  this.load.image('ground', 'assets/img/ground.png');
+  this.load.image('star', 'assets/img/star.png');
+  this.load.image('bomb', 'assets/img/bomb.png');
+  this.load.spritesheet('char',
+    'assets/img/char.png',
+    { frameWidth: 32, frameHeight: 48 }
+  );
 }
 
 function create ()
